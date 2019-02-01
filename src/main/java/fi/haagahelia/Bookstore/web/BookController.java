265b393905f8b5c2d3b363480a0383163c342a17
changHyunKim <kraze3704@gmail.com>
2019-02-01 22:31:35 +0200

    added delete feature
    BookController
    > created a mapping to /delete/{id}
    > in the booklist.html each tr has a delete link with its id

--- src/main/java/fi/haagahelia/Bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/Bookstore/web/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,6 +39,12 @@ public class BookController {
 	public String addBookPOST(Book book) {
 		bookRepo.save(book);
 		return "redirect:booklist";
+	}
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId) {
+		bookRepo.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 	
 	@Bean
